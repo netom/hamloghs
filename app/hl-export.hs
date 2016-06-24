ac185e09334cbe4f0e54e3e439f1264b19622966
Fábián Tamás László <giganetom@gmail.com>
2016-06-24 21:03:40 +0200

    Experiments with optparse-applicative

--- app/hl-export.hs
+++ app/hl-export.hs
@@ -7,23 +7,22 @@ data DatabaseOptions = DatabaseOptions
   { basedir :: String
   }
 
-databaseOptions :: Parser DatabaseOptions
-databaseOptions = DatabaseOptions
-  <$> strOption
+databaseOptions :: ParserInfo DatabaseOptions
+databaseOptions = info (helper <*> (DatabaseOptions
+  <$> strOption -- TODO: options should go into the Lib
       ( long "basedir"
      <> value "~/.hamloghs/data"
      <> short 'b'
      <> metavar "BASEDIR"
      <> help "The root directod of the " )
+  ))
+  ( fullDesc
+         <> progDesc "Print a greeting for TARGET"
+         <> header "hello - a test for optparse-applicative" )
 
 greet :: DatabaseOptions -> IO ()
 greet dbo = putStrLn $ "Using " ++ basedir dbo
 
 main :: IO ()
 main = do
-    execParser opts >>= greet
-      where
-        opts = info (helper <*> databaseOptions)
-          ( fullDesc
-         <> progDesc "Print a greeting for TARGET"
-         <> header "hello - a test for optparse-applicative" )
+    execParser databaseOptions >>= greet
